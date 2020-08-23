e972f3247d4a9c98e712341dab6503230ffd5bfa
Lucas <lucasdonuts@gmail.com>
2020-08-22 20:15:15 -0500

    Cleaned up and added lots of comments

--- stock_picker.rb
+++ stock_picker.rb
@@ -4,34 +4,41 @@ def stock_picker(stocks)
   result = []
   profit = 0
   comp_arr = []
+  # k will help to keep nil values out of our comparison array
   k = 1
 
-  stocks.each_with_index do |day, idx|
-    buy_day = idx
-    i = idx + 1
+  # iterate through the array, assigning the value as the stock price and the index as the day
+  stocks.each_with_index do |price, day|
+    i = day + 1
     diff_arr = []
 
-    if(idx == stocks.length - 1)
+    # an if statement to prevent the last day from being evaluated
+    if(day == stocks.length - 1)
       next
     end
+
     #while loop compares each day to the rest
     while i < stocks.length
-      if(day <= stocks[i])
-        difference = stocks[i] - day
-        diff_arr[i-k] = [difference, buy_day, i]
+      if(price <= stocks[i]) # to prevent negative numbers
+        profit = stocks[i] - price
+        diff_arr[i-k] = [profit, day, i]
         i += 1
       else
-        difference = 0
-        diff_arr[i-k] = [difference, buy_day, i]
+        profit = 0
+        diff_arr[i-k] = [profit, day, i]
         i += 1
       end
     end
     k += 1
     
+    # sorting puts the highest profit sell at the end of the array
     diff_arr.sort!
-    comp_arr[idx] = diff_arr.last
+    # this puts the highest profit sell for this day in a new array to be compared later
+    comp_arr[day] = diff_arr.last
   end
+  # again, sorting moves the most profitable sale to the end of the array
   comp_arr.sort!
+  # stores the buy day and sell day of the most profitable sale into the result variable
   result = comp_arr.last.last(2)
 end
 
